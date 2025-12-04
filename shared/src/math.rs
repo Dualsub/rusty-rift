6dcfad17fda33e453da5dfc9b61ffd43aacd146b
Simon Sjöling <rizedualsub@gmail.com>
2025-12-04 08:22:19 +0100

    Math utils

--- shared/src/math.rs
+++ shared/src/math.rs
@@ -1,6 +1,7 @@
-pub use glam::{Mat4, Vec2, Vec3};
+pub use glam::{Mat4, Vec2, Vec3, Vec4};
 
 pub type Mat4Data = [f32; 16];
+pub type Vec4Data = [f32; 4];
 
 pub trait ToData<T> {
     fn to_data(&self) -> T;
@@ -9,5 +10,11 @@ pub trait ToData<T> {
 impl ToData<Mat4Data> for Mat4 {
     fn to_data(&self) -> Mat4Data {
         self.to_cols_array()
+    }
+}
+
+impl ToData<Vec4Data> for Vec4 {
+    fn to_data(&self) -> Vec4Data {
+        self.to_array()
     }
 }
