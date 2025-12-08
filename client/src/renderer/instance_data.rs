c4ae02c290a75104a846f8fc057250697ca9407e
Simon Sjöling <rizedualsub@gmail.com>
2025-12-08 11:54:55 +0100

    Add some instance data for textures and colors

--- client/src/renderer/instance_data.rs
+++ client/src/renderer/instance_data.rs
@@ -4,4 +4,7 @@ use shared::math::*;
 #[derive(Default, Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
 pub struct StaticInstanceData {
     pub(crate) model_matrix: Mat4Data,
+    pub(crate) color: Vec4Data,
+    pub(crate) tex_coord: Vec2Data,
+    pub(crate) tex_scale: Vec2Data,
 }
